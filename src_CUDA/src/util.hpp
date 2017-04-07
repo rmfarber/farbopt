d0e0c7321adfc670ff2acbdd40c036416a883559
Rob Farber <rmfarber@gmail.com>
2017-04-07 09:50:58 -0700

    partial update for mpi and checkpointing

--- src_CUDA/src/util.hpp
+++ src_CUDA/src/util.hpp
@@ -13,6 +13,24 @@
 #include <omp.h>
 using namespace std;
 
+#ifdef USE_MPI
+
+#warning "MPI not tested on real hardware"
+#include "mpi.h"
+
+inline int getMPI_rank() {
+  int mpiRank;;
+  MPI_Comm_rank(MPI_COMM_WORLD,&mpiRank);
+  return mpiRank;;
+}
+
+inline int getMPI_tasks() {
+  int numTasks;
+  MPI_Comm_size(MPI_COMM_WORLD,&numTasks);
+  return numTasks;
+}
+#endif
+
 inline double getTime() { return(omp_get_wtime());}
 
 
@@ -24,6 +42,12 @@ __global__ void launchObjKernel( ObjFcn<float,
 
 template< typename REAL_T, typename myFcnInterest >
 struct ObjFuncVec {
+private:
+  string paramFilename;
+  double prevTime, last_err;
+  int checkpointInterval;
+
+public:
   int nParam;
   REAL_T *param;
   vector< ObjFcn<REAL_T, myFcnInterest>* > vec;
@@ -41,13 +65,165 @@ struct ObjFuncVec {
 
     nFunctionCalls = 0;
     minTime = maxTime = dataLoadTime = timeObjFunction = 0.;
+    checkpointInterval=0; // default is no checkpoint
+    prevTime = 0.;
+    last_err=0.;
   }
   ~ObjFuncVec()
   {
     cudaFreeHost(param);
   }
+  void setParamFilename(const char *fname)
+  {
+    paramFilename = fname;
+    paramFilename.erase(paramFilename.find_last_of(".", string::npos));
+  }
+  // checkpoint every interval of runtime
+  void setCheckPointInterval(int interval_seconds)
+  {
+    if(interval_seconds < 0) throw "invalid interval_seconds specified";
+#ifdef USE_MPI
+    if(getMPI_rank() == 0) { // master
+      checkpointInterval = interval_seconds;
+    }
+#else
+    checkpointInterval = interval_seconds;
+#endif
+  }
+
+  string getParamCheckPointName()
+  {
+    time_t rawtime;
+    struct tm timeinfo;
+    
+    time (&rawtime);
+    gmtime_r (&rawtime, &timeinfo);
+    
+    char buffer [80];
+    strftime(buffer, 80, ".utc-%Y_%m_%d_%H_%M_%S", &timeinfo);
+    char err_s[80];
+    sprintf(err_s, ".err-%.10lf",err);
+    return(paramFilename+buffer+err_s+".dat");
+  }
+  void writeCheckpoint(double curTime)
+  {
+    if(last_err == 0.) last_err = err;
+
+    if(checkpointInterval && nFunctionCalls && (err <= last_err) 
+       && (((curTime - prevTime) >= checkpointInterval) || curTime==0.)) {
+      extern void writeParam(const char *, int , float *);
+      writeParam(getParamCheckPointName().c_str(),nParam, param);
+      prevTime = getTime();
+      last_err = err;
+    }
+  }
+  void cuda_PartitionLoadData(uint32_t nExamples, bool hostOnly, uint32_t nInput, uint32_t nOutput, FILE *fn)
+  {
+    int ret;
+    vector<pair<int,int> > examplesPerDevice;
+    
+    // construct example vector
+    if(hostOnly) {
+      //Note: set from command-line to size for hybrid CPU/CPU compute
+      examplesPerDevice.push_back(make_pair(-1,nExamples));
+    }
+    
+    vector<cudaDeviceProp> propsVec;
+    if(!hostOnly) {
+      int nDevices;
+      cudaGetDeviceCount(&nDevices);
+      int totalCudaExamples=nExamples;
+      // subtract off all host examples
+      for(int i=0; i < examplesPerDevice.size(); i++)
+	totalCudaExamples -= examplesPerDevice[i].second;
+      assert(totalCudaExamples > 0);
+      
+      // Get device properties
+      for(int dev=0; dev < nDevices; dev++) {
+	cudaDeviceProp props;
+#ifdef USE_CUDA_DEVICE
+#warning "USING a single CUDA device"
+	if(dev != USE_CUDA_DEVICE) continue;
+#endif
+	cudaSetDevice(dev);
+	assert(cudaError() == cudaSuccess);
+	cudaGetDeviceProperties(&props, dev);
+	assert(cudaError() == cudaSuccess);
+	propsVec.push_back(props);
+      }
+      
+      // find number of active blocks to decide how many examples per device
+      int totalActiveBlk=0;
+      for(int i=0; i < propsVec.size(); i++) {
+	int nBlocks = propsVec[i].maxThreadsPerBlock/WARP_SIZE
+	  * propsVec[i].multiProcessorCount;
+	totalActiveBlk += nBlocks;
+      }
+      
+      int examplesPerBlock = totalCudaExamples/totalActiveBlk; 
+      for(int i=propsVec.size()-1; i >= 0; i--) {
+	// Ignore devices less than 3.5
+	if(propsVec[i].major < MIN_ARCH_MAJOR) continue;
+	if(propsVec[i].major == MIN_ARCH_MAJOR
+	   && propsVec[i].minor < MIN_ARCH_MINOR) continue;
+	
+	int nBlocks = propsVec[i].maxThreadsPerBlock/WARP_SIZE
+	  * propsVec[i].multiProcessorCount;
+	
+	int n = nBlocks * examplesPerBlock;
+	
+	totalCudaExamples -= n;
+	assert(totalCudaExamples >= 0);
+	if(i==0) {
+	  examplesPerDevice.push_back(make_pair(i,n+totalCudaExamples));
+	} else if(n>0) {
+	  examplesPerDevice.push_back(make_pair(i,n));
+	}
+      }
+    }
+    
+    // have examplesPerDevice constructed so read data into host-side oFunc
+    for(int i=0; i < examplesPerDevice.size(); i++) {
+      int myExamples = examplesPerDevice[i].second;
+      assert(myExamples > 0);
+      ObjFcn< REAL_T, myFcnInterest > *oFunc =
+	new ObjFcn< REAL_T, myFcnInterest >(myExamples);
+      
+      // read the data
+      for(int exIndex=0; exIndex < myExamples; exIndex++) {
+	for(int i=0; i < nInput; i++) {
+	  ret=fread(& oFunc->InputExample(exIndex,i),1, sizeof(REAL_T), fn);
+	  if(ret != sizeof(REAL_T)) throw "data read failed";
+	}
+	for(int i=0; i < nOutput; i++)  {
+	  ret=fread(& oFunc->KnownExample(exIndex,i),1, sizeof(REAL_T), fn);
+	  if(ret != sizeof(REAL_T)) throw "data read failed";
+	}
+      }
+      int dev = examplesPerDevice[i].first;
+      
+      if(examplesPerDevice[i].first < 0) {
+	this->vec.push_back( oFunc ); 
+      } else { // CUDA device
+	oFunc->warpSize = propsVec[dev].warpSize;
+	oFunc->multiProcessorCount = propsVec[dev].multiProcessorCount;
+	oFunc->maxThreadsPerBlock = propsVec[dev].maxThreadsPerBlock;
+	
+	this->vec.push_back(new ObjFcn< REAL_T, myFcnInterest >(*oFunc, dev));
+	
+	printf("\tDevice %d: \"%s\" with Compute %d.%d capability\n",
+	       dev, propsVec[dev].name,
+	       propsVec[dev].major, propsVec[dev].minor);
+	printf("\tWarpSize %d, nSMX %d,maxThreadsPerBlock %d\n",
+	       oFunc->warpSize, oFunc->multiProcessorCount, 
+	       oFunc->maxThreadsPerBlock);
+	
+	delete oFunc;
+      }
+    }
+  }
 };
-  
+
 extern "C" double nloptFunc(unsigned int n, const double *x,
 			    double *grad, void* f_data)
 {
@@ -55,8 +231,16 @@ extern "C" double nloptFunc(unsigned int n, const double *x,
     *oFuncVec = (ObjFuncVec<float, generatedFcnInterest<float> > *) f_data;
   
   assert(n == oFuncVec->nParam);
-
+  
   double startTime = getTime();
+  
+#ifdef USE_MPI
+  if(getMPI_rank() == 0) { // master
+    int op=1;
+    MPI_Bcast(&op, 1, MPI_INT, 0, MPI_COMM_WORLD); // Send the master op code
+    MPI_Bcast((void*) x, N_PARAM, MPI_DOUBLE, 0, MPI_COMM_WORLD); // Send the parameters
+  }
+#endif
 
 #pragma SIMD
   for(int i=0; i < n; ++i)
@@ -109,6 +293,16 @@ extern "C" double nloptFunc(unsigned int n, const double *x,
     err += oFunc->myErr;
   }
   
+#ifdef USE_MPI
+  double partialError = err;
+  double totalError=0.;
+  if(getMPI_rank() == 0) { // master
+    MPI_Reduce(&partialError, &totalError, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD); // get the totalError
+    err = totalError;
+  }
+#endif
+  
+  double curTime = getTime();
   double timeDelta = getTime() - startTime;
   if(oFuncVec->nFunctionCalls == 1) { // ignore 0 function call
     oFuncVec->minTime = oFuncVec->maxTime = timeDelta;
@@ -119,8 +313,30 @@ extern "C" double nloptFunc(unsigned int n, const double *x,
   oFuncVec->nFunctionCalls++;
   oFuncVec->err = err;
 
+  oFuncVec->writeCheckpoint(curTime);
+
   return err;
 }
+
+#ifdef USE_MPI
+void startClient(double *xFromMPI, void *my_func_data)
+{
+  int op;
+  double partialError,sum;
+
+  
+  for(;;) { // loop until the master says I am done
+    MPI_Bcast(&op, 1, MPI_INT, 0, MPI_COMM_WORLD); // receive the op code
+    if(op==0) { // we are done, normal exit
+      break;
+    }
+    MPI_Bcast(xFromMPI, N_PARAM, MPI_DOUBLE, 0, MPI_COMM_WORLD); // receive the parameters
+    partialError = nloptFunc(N_PARAM,  xFromMPI, NULL, my_func_data);
+    MPI_Reduce(&partialError, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  }
+  MPI_Finalize();
+}
+#endif
 
 int readParam(const char* filename, int nParam, float* param)
 {
@@ -168,6 +384,17 @@ ObjFuncVec<REAL_T, myFcnInterest >* init( const char* datafile,
 {
   FILE *fn=stdin;
 
+#ifdef USE_MPI
+  {
+    int ret = MPI_Init(NULL,NULL);
+    
+    if (ret != MPI_SUCCESS) {
+      printf ("Error in MPI_Init()!\n");
+      MPI_Abort(MPI_COMM_WORLD, ret);
+    }
+  }
+#endif
+  
   cout << "*******************" << endl;
 
   ObjFuncVec<REAL_T, myFcnInterest >  *oFuncVec
@@ -180,6 +407,8 @@ ObjFuncVec<REAL_T, myFcnInterest >* init( const char* datafile,
     fprintf(stderr,"Cannot open %s\n",datafile);
     exit(1);
   }
+
+  oFuncVec->setParamFilename(paramfile);
 
   // read the header information
   double startTime=getTime();
@@ -195,125 +424,97 @@ ObjFuncVec<REAL_T, myFcnInterest >* init( const char* datafile,
   ret=fread(&nExamples,sizeof(uint32_t), 1, fn);
   assert(ret == 1);
 
+#ifdef DEBUG_DATA_PARTITIONING
+  {
+    FILE *fn=fopen("data.hdr","w");
+    ret=fwrite(&nInput,sizeof(uint32_t), 1, fn);
+    assert(ret == 1);
+    ret=fwrite(&nOutput,sizeof(uint32_t), 1, fn);
+    assert(ret == 1);
+    ret=fwrite(&nExamples,sizeof(uint32_t), 1, fn);
+    fclose(fn);
+  }
+#endif
+
+#ifdef USE_MPI
+  int nTasks = getMPI_tasks();
+  rank = getMPI_rank();
+
+  if(rank==0) { // master
+    cout << "nInput " << nInput
+	 << " nOutput " << nOutput
+	 << " nExamples " << nExamples
+	 << " in datafile (" << datafile << ")"
+	 << endl;
+    cout << "*******************" << endl;
+#pragma omp parallel
+    if(omp_get_thread_num() == 0)
+      cout << "Using MPI" << " numTasks " <<  nTasks << " OMP_NUM_THREADS " << omp_get_num_threads() << endl;
+  }
+  
+  {
+    // Set rankExamples vector to the number of examples per rank.
+    vector<int> rankExample(nTasks);
+    for(int i=0; i < nTasks; i++) rankExample[i] = nExamples/nTasks;
+
+    for(int i=0; i < nExamples % nTasks; i++) rankExample[i]++;
+
+    uint32_t offset=0;
+    for(int i=0; i < rank; i++) offset += rankExample[i];
+    // test if seekable
+    // seek to location
+    if(fseek(fn, (nInput+nOutput)*offset*sizeof(REAL_T), SEEK_CUR) != 0) {
+      cerr << "Cannot seek to location" << endl;
+      exit(1);
+    }
+    
+    if(rank==0) { // master
+      cout << "\tExamples MPI rank 0 " << rankExample[0] 
+	   << "\tExamples MPI rank " << (nTasks-1) << " " << rankExample[nTasks-1] << endl; 
+    }
+
+    offset=0;
+    for(int i=0; i < nTasks; i++) offset += rankExample[i];
+    assert(offset == nExamples);
+
+    // set nExamples to the right one for the current rank
+    nExamples = rankExample[rank];
+  }
+#else
+#pragma omp parallel
+  if(omp_get_thread_num() == 0)
+    cout << "OMP_NUM_THREADS " << omp_get_num_threads() << endl;
   cout << "nInput " << nInput
        << " nOutput " << nOutput
        << " nExamples " << nExamples
        << " in datafile (" << datafile << ")"
        << endl;
+  cout << "*******************" << endl;
+#endif
+
 #ifdef LAYOUT_SOA
   cout << "Using SOA layout" << endl;
+#endif
+
+  oFuncVec->cuda_PartitionLoadData(nExamples, hostOnly, nInput, nOutput,fn);
+
+  oFuncVec->dataLoadTime = getTime() - startTime;
+
+  if(fn!=stdin)
+    fclose(fn);
+
+#ifdef USE_MPI
+  if(getMPI_rank() > 0) {
+    vector<double> x(N_PARAM);
+    startClient(&x[0], (void *) oFuncVec);
+    exit(0); // normal client exit
+  }
 #endif
 
   //read parameter file if it exists otherwise randomize the paramters
   for(int i=0; i < oFuncVec->nParam; i++) oFuncVec->param[i]
 					    = 0.4*(rand()/(double)RAND_MAX);
   readParam(paramfile, oFuncVec->nParam, oFuncVec->param);
-
-  vector<pair<int,int> > examplesPerDevice;
-
-  // construct example vector
-  if(hostOnly) {
-    //Note: set from command-line to size for hybrid CPU/CPU compute
-    examplesPerDevice.push_back(make_pair(-1,nExamples));
-  }
-
-  vector<cudaDeviceProp> propsVec;
-  if(!hostOnly) {
-    int nDevices;
-    cudaGetDeviceCount(&nDevices);
-    int totalCudaExamples=nExamples;
-    // subtract off all host examples
-    for(int i=0; i < examplesPerDevice.size(); i++)
-      totalCudaExamples -= examplesPerDevice[i].second;
-    assert(totalCudaExamples > 0);
-
-    // Get device properties
-    for(int dev=0; dev < nDevices; dev++) {
-      cudaDeviceProp props;
-#ifdef USE_CUDA_DEVICE
-#warning "USING a single CUDA device"
-      if(dev != USE_CUDA_DEVICE) continue;
-#endif
-      cudaSetDevice(dev);
-      assert(cudaError() == cudaSuccess);
-      cudaGetDeviceProperties(&props, dev);
-      assert(cudaError() == cudaSuccess);
-      propsVec.push_back(props);
-    }
-
-    // find number of active blocks to decide how many examples per device
-    int totalActiveBlk=0;
-    for(int i=0; i < propsVec.size(); i++) {
-      int nBlocks = propsVec[i].maxThreadsPerBlock/WARP_SIZE
-	* propsVec[i].multiProcessorCount;
-      totalActiveBlk += nBlocks;
-    }
-
-    int examplesPerBlock = totalCudaExamples/totalActiveBlk; 
-    for(int i=propsVec.size()-1; i >= 0; i--) {
-      // Ignore devices less than 3.5
-      if(propsVec[i].major < MIN_ARCH_MAJOR) continue;
-      if(propsVec[i].major == MIN_ARCH_MAJOR
-	 && propsVec[i].minor < MIN_ARCH_MINOR) continue;
-      
-      int nBlocks = propsVec[i].maxThreadsPerBlock/WARP_SIZE
-	* propsVec[i].multiProcessorCount;
-
-      int n = nBlocks * examplesPerBlock;
-
-      totalCudaExamples -= n;
-      assert(totalCudaExamples >= 0);
-      if(i==0) {
-	examplesPerDevice.push_back(make_pair(i,n+totalCudaExamples));
-      } else if(n>0) {
-	examplesPerDevice.push_back(make_pair(i,n));
-      }
-    }
-  }
-    
-  // have examplesPerDevice constructed so read data into host-side oFunc
-  for(int i=0; i < examplesPerDevice.size(); i++) {
-    int myExamples = examplesPerDevice[i].second;
-    assert(myExamples > 0);
-    ObjFcn< REAL_T, myFcnInterest > *oFunc =
-      new ObjFcn< REAL_T, myFcnInterest >(myExamples);
-    
-    // read the data
-    for(int exIndex=0; exIndex < myExamples; exIndex++) {
-      for(int i=0; i < nInput; i++) {
-	ret=fread(& oFunc->InputExample(exIndex,i),1, sizeof(REAL_T), fn);
-	if(ret != sizeof(REAL_T)) throw "data read failed";
-      }
-      for(int i=0; i < nOutput; i++)  {
-	ret=fread(& oFunc->KnownExample(exIndex,i),1, sizeof(REAL_T), fn);
-	if(ret != sizeof(REAL_T)) throw "data read failed";
-      }
-    }
-    int dev = examplesPerDevice[i].first;
-
-    if(examplesPerDevice[i].first < 0) {
-      oFuncVec->vec.push_back( oFunc ); 
-    } else { // CUDA device
-      oFunc->warpSize = propsVec[dev].warpSize;
-      oFunc->multiProcessorCount = propsVec[dev].multiProcessorCount;
-      oFunc->maxThreadsPerBlock = propsVec[dev].maxThreadsPerBlock;
-
-      oFuncVec->vec.push_back(new ObjFcn< REAL_T, myFcnInterest >(*oFunc, dev));
-
-      printf("\tDevice %d: \"%s\" with Compute %d.%d capability\n",
-	     dev, propsVec[dev].name,
-	     propsVec[dev].major, propsVec[dev].minor);
-      printf("\tWarpSize %d, nSMX %d,maxThreadsPerBlock %d\n",
-	     oFunc->warpSize, oFunc->multiProcessorCount, 
-	     oFunc->maxThreadsPerBlock);
-      
-      delete oFunc;
-    }
-  }
-  oFuncVec->dataLoadTime = getTime() - startTime;
-
-  if(fn!=stdin)
-    fclose(fn);
 
   assert(oFuncVec->vec.size() > 0);
   {
@@ -354,6 +555,14 @@ void fini(const char * paramFilename,
 #endif
 
   writeParam(paramFilename, oFuncVec->nParam, oFuncVec->param);
+  oFuncVec->writeCheckpoint(0.);
+#ifdef USE_MPI
+  {
+    int op=0;
+    MPI_Bcast(&op, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Finalize();
+  }
+#endif
 
   delete oFuncVec;
 }
