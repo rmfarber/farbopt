44949b351009799485913c3f78c69d0c509d7707
Rob Farber <rmfarber@gmail.com>
2017-04-25 19:38:30 -0700

    added ELU G() func

--- src/Gfcn.h
+++ src/Gfcn.h
@@ -27,6 +27,11 @@ inline float G(float x) { return( tanhf(x) ) ;}
 #define G_DESC_STRING "logistic()"
 FCN_ATTRIBUTES
 inline float G(float x) { return( 1.f/(1.f+expf(-x)) ) ;} 
+#elif ELU
+
+#define G_DESC_STRING "Exponential linear unit(parameter a locked to 1.)"
+FCN_ATTRIBUTES
+inline float G(float x) { return( (x>0.f)?x:(expf(x)-1.f) ); } 
 #define G_ESTIMATE 7 // estimate flops for G
 
 #else // Use linear
