7c2e6d2a06b98f97e3548e4882f00537eb572e35
Rob Farber <rmfarber@gmail.com>
2018-10-25 12:44:14 -0700

    add estimate message to Gfcn

--- src/Gfcn.h
+++ src/Gfcn.h
@@ -17,21 +17,21 @@ inline float G(float x) { return( x/(1.f+fabsf(x)) ) ;}
 
 #elif defined(TANH_ACTIVATION_FCN)
 
-#define G_DESC_STRING "tanh()"
+#define G_DESC_STRING "tanh() estimated 7 flops to calc G"
 FCN_ATTRIBUTES
 inline float G(float x) { return( tanhf(x) ) ;} 
 #define G_ESTIMATE 7 // estimate 7 flops for G
 
 #elif defined(LOGISTIC_ACTIVATION_FCN)
 
-#define G_DESC_STRING "logistic()"
+#define G_DESC_STRING "logistic() estimated 9 flops to calc G"
 FCN_ATTRIBUTES
 inline float G(float x) { return( 1.f/(1.f+expf(-x)) ) ;} 
 #define G_ESTIMATE 9 // estimate 7 flops for G
 
 #elif defined(ELU_ACTIVATION_FCN)
 
-#define G_DESC_STRING "Exponential linear unit(parameter a locked to 1.)"
+#define G_DESC_STRING "Exponential linear unit(parameter a locked to 1.) estimated 7 flops to calc G"
 FCN_ATTRIBUTES
 inline float G(float x) { return( (x>0.f)?x:(expf(x)-1.f) ); } 
 #define G_ESTIMATE 7 // estimate flops for G
